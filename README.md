7247703a8896f134ae724b9f4091d77aef554291
Brian joel Almiron <brn_joel@hotmail.com>
2026-02-03 20:14:21 -0300

    estilos

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# mokepon
+# crea un puntero de un elemento en html a traves de su ID.
+let variable = document.getElementById("nombre del ID");
+
+# Este metodo nos sirve para verificar y devolver un valor booleano si el elemento esta seleccionado o no por el usuario o si el evento se hizo uso.
+let variable = document.getElementById("nombre del ID").checked;  (devuelve true o false)
+
+# Es un metodo que podemos asigarle al puntero que nos permite escuchar eventos y luego hacer una accion, no piden 2 parametros para que funciones. (evento , funcion)
+variable.addEventListener('click', funcion);
+
+# Esta metodo nos sirve para cambiar o agregar una palabra en HTML desde JS.Tenemos que primer crear un puntero a una etiqueta con un ID , generalmente un <span id="">, y luego aplicar el metodo para agregarle texto. 
+variable.innerHTML="texto";
+
+/////////////////////////////////////////////////////////////////////////////////////////
+# 1.Crea un etiqueta desde JS para HTML.
+var variable = document.createElement("p");
+# 2.Crea un texto en el cual podemos asignar luego en html, similar a .innerHTML="texto", pero esta nos permite crea de manera mas eficiente mas texto.
+var variableContent = document.createTextNode("Hola!¿Qué tal?");
+# 3.este metodo introduce el texto creado por <document.createTextNode("Hola!¿Qué");> en la variable creada con el metodo <document.createElement("p",[options]);>
+variable.appendChild(variableContent); //añade texto al p creado.
+# 4. añade el elemento creado y su contenido al DOM
+# creamos un puntero a la seccion que queremos ubicar la nueva etiqueta gracias a su ID.
+var variableP = document.getElementById("idP");
+# 5. Usamos el metodo <document.body.insertBefore(variable,variableP);> e ingreamos los 2 parametros, el 1° es la variable donde creamos el elemento HTML y el 2° es la variable donde tenemos señalizada con un puntero la ubicacion donde queremos que se coloque nuestra nueva etiqueta en nuestro HTML.
+document.body.insertBefore(variable,variableP);
+
+VARIANTE
+ //creamos el puntero a la etiqueta en HTML
+ <seccionMensaje=document.getElementById("mensaje");>
+ //Utilizamos el metodo "createElement" y luego con el metodo ".innerHTML" introducimos el texto en esa etiqueta.
+ <let parrafo=document.createElement("p") >
+ <parrafo.innerHTML=("Tu mascota atacó con "+ataqueJugador+", las mascota del enemigo atacó con "+ataqueEnemigo+" - PENDIENTE");>
+//seleccionamos la variable donde tenemos el puntero y llamamos al metodo ".appendChild" que inserta la ETIQUETA JUNTO AL MENSAJE en la seccion seleccionada.
+ <seccionMensaje.appendChild(parrafo);>
+////////////////////////////////////////////////////////////////////////////////////////////
--- js/pokemon.js
+++ js/pokemon.js
@@ -1,18 +1,25 @@
+let ataqueJugador;
+let ataqueEnemigo;
+let vidasJugador=3;
+let vidasEnemigo=3;
 function iniciarJuego() {
+    let seccionMostrarAtaque=document.getElementById("selection-atack");
+    seccionMostrarAtaque.style.display = 'none'; // Oculta ataques
+
+    let seccionMostrarReinicio=document.getElementById("reiniciar");
+    seccionMostrarReinicio.style.display = 'none'; // ocultar reinicio
     //Mascota
     let botonMascotaJugador = document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
-    
-    //Ataque jugador
-    let ataqueJugador;
-    let ataqueEnemigo;
-
     let botonFuego=document.getElementById("boton-fuego");
     botonFuego.addEventListener("click",ataqueFuego);
     let botonAgua=document.getElementById("boton-agua");
     botonAgua.addEventListener("click",ataqueAgua);
     let botonTierra=document.getElementById("boton-tierra");
     botonTierra.addEventListener("click",ataqueTierra);
+
+    let botonReiniciar=document.getElementById("reiniciar");
+    botonReiniciar.addEventListener("click",reiniciarJuego);
 }
 
 function seleccionarMascotaJugador() {
@@ -20,6 +27,11 @@ function seleccionarMascotaJugador() {
     let capipepoSelect=document.getElementById("capipepo").checked;
     let ratigueyaSelect=document.getElementById("ratigueya").checked;
     let mascotaJugador=document.getElementById(`mokemonJugador`);
+    let jugadorVidas=document.getElementById("vidasJugador");
+    jugadorVidas.innerHTML=vidasJugador;
+    let pcVidas=document.getElementById("vidasEnemigo");
+    pcVidas.innerHTML=vidasEnemigo
+
     if(hipodogeSelect){
         mascotaJugador.innerHTML="Hipodoge";
     }else if(capipepoSelect){
@@ -30,6 +42,10 @@ function seleccionarMascotaJugador() {
      alert("Necesitas seleccionar una mascota primero");   
     }
     seleccionarMascotaPC();
+    let seccionMostrarAtaque=document.getElementById("selection-atack");
+    seccionMostrarAtaque.style.display = 'block'; // muestra
+    let seccionMostrarMascota=document.getElementById("selection-mokemon");
+    seccionMostrarMascota.style.display="none"; 
 }
 
 function seleccionarMascotaPC () {
@@ -42,6 +58,7 @@ function seleccionarMascotaPC () {
     }else{
     mokemonPC.innerHTML="Ratigueya"
   }
+
 }
 
 function aleatorio (min,max){
@@ -49,43 +66,124 @@ return Math.floor( Math.random() * (max-min+1) + min);
 }
 
 function ataqueFuego(){
-    let mensajeAtaqueJugador=document.getElementById("ataque-jugador");
     ataqueJugador="fuego";
-    mensajeAtaqueJugador.innerHTML=ataqueJugador;
     seleccionAtaquePC();
+    if (vidasEnemigo>0&&vidasJugador>0) {
+        mostrarMensaje();
+    }else{
+        verificarVidas();
+    }
 }
 function ataqueAgua(){
     ataqueJugador="agua";
-    let mensajeAtaqueJugador=document.getElementById("ataque-jugador");
-    mensajeAtaqueJugador.innerHTML=ataqueJugador;
     seleccionAtaquePC();
+    if (vidasEnemigo>0&&vidasJugador>0) {
+        mostrarMensaje();
+    }else{
+        verificarVidas();
+    }
   
 }
 function ataqueTierra(){
     ataqueJugador="tierra";
-    let mensajeAtaqueJugador=document.getElementById("ataque-jugador");
-    mensajeAtaqueJugador.innerHTML=ataqueJugador;
     seleccionAtaquePC();
+    if (vidasEnemigo>0&&vidasJugador>0) {
+        mostrarMensaje();
+    }else{
+        verificarVidas();
+    }
+}
+
+function verificarVidas (){
+    if(vidasJugador==0){
+        mostrarMensajeFinal("Perdiste!");
+    }else if(vidasEnemigo == 0){
+        mostrarMensajeFinal("Ganaste!!!");
+    }
+    let botonFuego=document.getElementById("boton-fuego");
+    botonFuego.disabled=true;
+    let botonAgua=document.getElementById("boton-agua");
+    botonAgua.disabled=true;
+    let botonTierra=document.getElementById("boton-tierra");
+    botonTierra.disabled=true;
+
+    let seccionMostrarReinicio=document.getElementById("reiniciar");
+    seccionMostrarReinicio.style.display = 'block'; // muestra    
+
+}
+
+function mostrarMensajeFinal(resultado){
+ seccionMensaje=document.getElementById("mensaje");
+ let parrafo = document.createElement("p");
+ parrafo.innerHTML=("Partida finalizada! : "+resultado );
+ seccionMensaje.appendChild(parrafo);
 }
 
 function seleccionAtaquePC(){
     let numAleatorio=aleatorio(1,3);
-    console.log(numAleatorio);
-    let mensajeAtaqueEnemigo=document.getElementById("ataque-enemigo");
 
     if (numAleatorio==1) {
-      ataqueEnemigo="Fuego";
-      mensajeAtaqueEnemigo.innerHTML=ataqueEnemigo;
-      console.log(ataqueEnemigo);
+      ataqueEnemigo="fuego";
   }else if (numAleatorio==2){
-      ataqueEnemigo="Agua";
-      console.log(ataqueEnemigo);
-      mensajeAtaqueEnemigo.innerHTML=ataqueEnemigo;
+      ataqueEnemigo="agua";
     }else{
-      ataqueEnemigo="Tierra";
-      console.log(ataqueEnemigo);
-      mensajeAtaqueEnemigo.innerHTML=ataqueEnemigo;
+      ataqueEnemigo="tierra";
   }
     
 }
-window.addEventListener('load', iniciarJuego)
+function mostrarMensaje(){
+ seccionMensaje=document.getElementById("mensaje");
+ console.log(vidasEnemigo)
+    
+ let parrafo = document.createElement("p");
+ let resultado = combate();
+ 
+ parrafo.innerHTML=("Tu mascota atacó con "+ataqueJugador+", las mascota del enemigo atacó con "+ataqueEnemigo+" - "+ resultado );
+
+ seccionMensaje.appendChild(parrafo);
+}
+
+
+function combate (){
+    let resultado="";
+    let vidasJ=document.getElementById("vidasJugador");
+    let vidasPC=document.getElementById("vidasEnemigo");
+
+    if(ataqueJugador==ataqueEnemigo){
+        resultado="empate";
+        return resultado
+    }
+
+    if(ataqueJugador=="fuego" && ataqueEnemigo=="tierra"){
+        resultado="ganaste";
+        if(vidasEnemigo>0){
+            vidasEnemigo--;
+            vidasPC.innerHTML=vidasEnemigo;
+        }
+        return resultado;
+    }else if(ataqueJugador=="tierra"&& ataqueEnemigo=="agua"){
+        resultado="ganaste";
+        if(vidasEnemigo>0){
+            vidasEnemigo--;
+            vidasPC.innerHTML=vidasEnemigo;}
+        return resultado;
+    }else if(ataqueJugador=="agua"&& ataqueEnemigo=="fuego"){
+        resultado="ganaste";
+        if(vidasEnemigo>0){
+            vidasEnemigo--;
+            vidasPC.innerHTML=vidasEnemigo;}
+        return resultado;
+    }else{
+        resultado="perdiste";
+        if(vidasJugador>0){
+            vidasJugador--;
+            vidasJ.innerHTML=vidasJugador;}
+        return resultado;
+    }
+}
+function reiniciarJuego(){
+    location.reload();
+}
+
+//todo 
+window.addEventListener("load", iniciarJuego);
